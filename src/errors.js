1645cecfd38a569638ab0a35271a4b82e59f9bf5
James Krayer <jkrayer@ABT-JK-H03461.local>
2018-01-05 09:16:21 -0500

    Add file to store error messages

---
+++ src/errors.js
@@ -0,0 +1,6 @@
+module.exports = {
+  CURRY_ERROR_MESSAGE: [
+    'curry expects the provided function to have at least one defined argument.',
+    'For multi-variate functions set the arity explicity as the second argument',
+    'to curry.'].join(' ')
+};
