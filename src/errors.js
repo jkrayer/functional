9b6f52d1a004f0166022b1ea80385ec34f84dadb
James Krayer <jkrayer@ABT-JK-H03461.home>
2018-01-07 01:32:19 -0500

    Add pipe

--- src/errors.js
+++ src/errors.js
@@ -2,5 +2,6 @@ module.exports = {
   CURRY_ERROR_MESSAGE: [
     'curry expects the provided function to have at least one defined argument.',
     'For multi-variate functions set the arity explicity as the second argument',
-    'to curry.'].join(' ')
+    'to curry.'].join(' '),
+  PIPE_ERROR_MESSAGE: 'All arguments provided to pipe must be functions'
 };
---
+++ src/pipe.js
@@ -0,0 +1,19 @@
+const PIPE_ERROR_MESSAGE = require('./errors').PIPE_ERROR_MESSAGE;
+
+/**
+ * Pipe saves an array of functions and returns a unary function. When the
+ * returned function is called the functions are applied to the value from
+ * left to right.
+ * @param  {[type]} fns Array of unary functions
+ * @return {[type]}     A unary function
+ */
+function pipe(...fns) {
+
+  if (!fns.every(fn => typeof fn === 'function')) {
+    throw new TypeError(PIPE_ERROR_MESSAGE);
+  }
+
+  return a => fns.reduce((acc, fn) => fn(acc), a);
+}
+
+module.exports = pipe;
