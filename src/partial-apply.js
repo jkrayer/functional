468f7ad59c272d545871fa93ddab008c21b5aaf2
James Krayer <jkrayer@ABT-JK-H03461.home>
2018-01-07 00:58:15 -0500

    Update documentation

--- src/partial-apply.js
+++ src/partial-apply.js
@@ -1,13 +1,23 @@
 /**
- * Save a function and any provided arguments. Return a function that can take
- * additional arguments. The returned function executes `fn` with all provided
- * arguments in order from left to right.
+ * partialApply creates a closure over the passed function and arguments and
+ * returns a function with access to the closure. When the returned function
+ * is called it returns the result of the original function with the initial
+ * and new arguments applied from left to right.
+ *
+ * function sum (a, b, c, d, e) {
+ *   return a + b + c + d + e;
+ * }
+ *
+ * const c = partialApply(sum, 3); // c is a lambda than can access sum and 3
+ * const d = c(1);                 // d is 4
+ * const e = c(2);                 // e is 5
+ *
  * @param  {Function} fn    a function to save for later
- * @param  {Array}    args  some or all of the arguments to use with `fn`
- * @return {Function}       a closure with access to `fn` and `args`
+ * @param  {Array}    args  some initial arguments to use with `fn`
+ * @return {Function}       a function with access to the closure created by partialApply
  */
 function partialApply(fn, ...args) {
-  return (...newArgs) => fn(...(args.concat(newArgs)));
+  return (...newArgs) => fn(...args.concat(newArgs));
 }
 
 module.exports = partialApply;
