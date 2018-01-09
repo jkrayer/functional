956df719d6f71871f964df0be4d65063e2d86922
James Krayer <jkrayer@ABT-JK-H03461.home>
2018-01-08 23:36:22 -0500

    Add index

---
+++ index.js
@@ -0,0 +1,11 @@
+const curry = require('./src/curry');
+const Maybe = require('./src/Maybe');
+const partial = require('./src/partial-apply');
+const pipe = require('./src/pipe');
+
+module.exports = {
+  curry,
+  Maybe,
+  partial,
+  pipe
+};
