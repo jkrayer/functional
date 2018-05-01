574dfd4c0f1724f708beb320d788c0e6fe194daa
James Krayer <jkrayer@ABT-JK-H03461.local>
2018-05-01 18:43:27 -0400

    Add once

---
+++ src/once.js
@@ -0,0 +1,15 @@
+/**
+ * Save a function in a closure that will only allow it to be called one time
+ * @param  {Function} fn a function to execute one time
+ * @return {Function}    a function that will call the provided function with
+ *                       the passed context and arguments
+ */
+function once(fn) {
+  let isDone = false;
+
+  return function() {
+    return isDone ? void 0 : ((isDone = true), fn.apply(this, arguments));
+  }
+}
+
+module.exports = once;
---
+++ tests/onceTest.js
@@ -0,0 +1,39 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const wonce = require('../src/once');
+
+require('chai').use(require('sinon-chai'));
+
+describe('wonce', () => {
+  it('should return function', () => {
+    const cb = sinon.spy();
+    const oneTime = wonce(cb);
+
+    expect(oneTime).to.be.an.instanceof(Function);
+  });
+
+  it('should execute the supplied function one time', () => {
+    const cb = sinon.spy();
+    const oneTime = wonce(cb);
+
+    oneTime();
+    oneTime();
+    oneTime();
+
+    expect(cb).to.have.been.calledOnce;
+  });
+
+  it('should execute the supplied function one time each time it is wrapped in wonce', () => {
+    const cb = sinon.spy();
+    const oneTime = wonce(cb);
+    const anotherTime = wonce(cb);
+
+    oneTime();
+    anotherTime();
+    oneTime();
+    anotherTime();
+
+    expect(cb).to.have.been.calledTwice;
+  });
+
+});
