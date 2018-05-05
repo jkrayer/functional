01c22a82473cd00058a4567bab8273ced02a223c
James Krayer <jkrayer@ABT-JK-H03461.home>
2018-05-04 22:29:44 -0400

    Add when

---
+++ src/when.js
@@ -0,0 +1,16 @@
+/**
+ * Save a function in a closure that will run only once on the nth attempt
+ * @param  {Function} fn a function to execute one time
+ * @param  {Integer}  x  fun the function after this amount of calls
+ * @return {Function}    a function that will call the provided function with
+ *                       the passed context and arguments
+ */
+function when(fn, x) {
+  let count = x;
+
+  return function() {
+    return --count === 0 ? fn.apply(this, arguments) : void 0;
+  };
+}
+
+module.exports = when;
---
+++ tests/whenTest.js
@@ -0,0 +1,42 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const when = require('../src/when');
+
+require('chai').use(require('sinon-chai'));
+
+describe('when', () => {
+  it('should return function', () => {
+    const cb = sinon.spy();
+    const w = when(cb, 1);
+
+    expect(w).to.be.an.instanceof(Function);
+  });
+
+  it('should execute the supplied function one time after 2 tries', () => {
+    const cb = sinon.spy();
+    const w = when(cb, 2);
+
+    w();
+
+    expect(cb).to.not.have.been.called;
+
+    w();
+    w();
+
+    expect(cb).to.have.been.calledOnce;
+  });
+
+  it('should execute the supplied function one time each time it is wrapped in when', () => {
+    const cb = sinon.spy();
+    const wOne = when(cb, 1);
+    const wTwo = when(cb, 1);
+
+    wOne();
+    wTwo();
+    wOne();
+    wTwo();
+
+    expect(cb).to.have.been.calledTwice;
+  });
+
+});
