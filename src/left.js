d402505e46942fb9f09ee5bc891756f66efc2aca
jkrayer <jameskrayer@yahoo.com>
2019-02-20 08:15:40 -0500

    Add left and right monads

---
+++ src/left.js
@@ -0,0 +1,33 @@
+const { isConstructor } = require('./helpers');
+// 1. https://blog.logrocket.com/elegant-error-handling-with-the-javascript-either-monad-76c7ae4924a1
+
+// This is the "error case" of Either
+function Left(a) {
+  if (!isConstructor(this, Left)) return Left.of(a); // Address need for instantiation with "new"
+
+  Object.defineProperty(this, 'v', { value: a, writable: false }); // 1.
+}
+
+Left.of = function of (a) {
+  return new Left(a);
+}
+
+Left.prototype.ap = function ap () {
+  return this;
+}
+
+Left.prototype.chain = function chain () {
+  return this;
+}
+
+Left.prototype.join = function () {
+  return this;
+}
+
+Left.prototype.map = function map () {
+  return this;
+}
+
+Left.prototype.toString = function toString() {
+  return `Left(${this.v.toString()})`;
+}
---
+++ src/right.js
@@ -0,0 +1,38 @@
+const { isConstructor } = require('./helpers');
+const Left = require './left';
+// 1. https://blog.logrocket.com/elegant-error-handling-with-the-javascript-either-monad-76c7ae4924a1
+
+// This is the "successr case" of Either
+function Right(a) {
+  if (!isConstructor(this, Right)) return Right.of(a); // Address need for instantiation with "new"
+
+  Object.defineProperty(this, 'v', { value: a, writable: false });
+}
+
+Right.of = function of (a) {
+  return new Right(a);
+}
+
+// Map the function supplied in the supplied either over this value
+Right.prototype.ap = function ap (either) {
+  const fn = either.v; // might more properly be .chain(identity)
+  return this.map(fn);
+}
+
+Right.prototype.chain = function chain (fn) {
+  return fn(this.v);
+}
+
+Right.prototype.join = function join () {
+  return (this.v instanceof Right || this.v instanceof Left)
+    ? this.v
+    : this;
+}
+
+Right.prototype.map = function map (fn) {
+  return Right.of(fn(this.v));
+}
+
+Right.prototype.toString = function toString() {
+  return `Right(${this.v.toString()})`;
+}
